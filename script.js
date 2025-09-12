36a783e3e8668ee485bfa797c00039bc9b52d779
Raman Shah <ramanshah440@gmail.com>
2025-09-12 15:34:24 +0545

    made box

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 let btn = document.getElementById("login");
-
-btn.addEventListener("click" ,() => {
+let notify = document.getElementById("notification");
+btn.addEventListener("click" ,(e) => {
+      e.preventDefault(); 
   let user= username.value;
     let pass= password.value;
-   alert(`verify its you ${user}`);
-     alert(`verify its your password  ${pass}`);
+   console.log("verify its you "+user);
+    
+ setTimeout(() => {
+    notify.style.display="block";
+   },1500);
 })
