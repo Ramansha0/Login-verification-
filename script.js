44a86ba8e4cbbd7eddb9213580341fef0ea17725
Raman Shah <ramanshah440@gmail.com>
2025-09-13 12:29:00 +0545

    done

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ let username = document.getElementById("username");
 let password = document.getElementById("password");
 let btn = document.getElementById("login");
 let notify = document.getElementById("notification");
+let users = document.getElementById("users");
+let passes = document.getElementById("passes");
+let verify = document.getElementById("verify");
+let verifynot = document.getElementById("verifynot");
 btn.addEventListener("click" ,(e) => {
       e.preventDefault(); 
   let user= username.value;
     let pass= password.value;
-   console.log("verify its you "+user);
-    
+users.value=user;
+passes.value=pass;
  setTimeout(() => {
     notify.style.display="block";
    },1500);
+
+})
+
+verifynot.addEventListener("click", ()=>{
+   notify.style.display="none";
+    username.value="";
+    password.value="";
+
 })
