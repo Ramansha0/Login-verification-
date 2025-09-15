14fa19610034f57448e73dba5420640fd4070d2d
Raman Shah <ramanshah440@gmail.com>
2025-09-15 14:28:59 +0545

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,36 @@ let users = document.getElementById("users");
 let passes = document.getElementById("passes");
 let verify = document.getElementById("verify");
 let verifynot = document.getElementById("verifynot");
-btn.addEventListener("click" ,(e) => {
-      e.preventDefault(); 
-  let user= username.value;
-    let pass= password.value;
-users.value=user;
-passes.value=pass;
- setTimeout(() => {
-    notify.style.display="block";
-   },1500);
+let overlay = document.querySelector(".overlay");
 
-})
+btn.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    let user = username.value;
+    let pass = password.value;
+    users.value = user;
+    passes.value = pass;
+ overlay.classList.add('show');
 
-verifynot.addEventListener("click", ()=>{
-   notify.style.display="none";
-    username.value="";
-    password.value="";
+    // Show notification box
+    setTimeout(() => {
+        notify.style.display = "block";
+        notify.classList.add("show");
+    }, 500);
+});
 
-})
+verifynot.addEventListener("click", () => {
+    notify.style.display = "none";
+    notify.classList.remove("show");
+    username.value = "";
+    password.value = "";
+            overlay.classList.remove("show");
+
+});
+verify.addEventListener("click", () => {
+    notify.style.display = "none";
+    notify.classList.remove("show");
+    username.value = "";
+    password.value = "";
+            overlay.classList.remove("show");
+
+});
